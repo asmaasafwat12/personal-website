ec8144114bd06a472cd1ad64640104068df8e700
asmaasafwat12 <67692349+asmaasafwat12@users.noreply.github.com>
2020-08-22 09:23:18 +0200

    Update script.js

--- script.js
+++ script.js
@@ -91,7 +91,6 @@ $(window).on('scroll', function () {
     //Animate progress-bar in About me section
     $(".progress .progress-bar").each(function () {
         var bottom_object = $(this).offset().top + $(this).outerHeight();
-        console.log($(this).offset().top + bottom_object);
         var bottom_window = $(window).scrollTop() + $(window).height();
         var progressWidth = $(this).attr('aria-valuenow') + '%';
         if (bottom_window > bottom_object) {
@@ -105,59 +104,3 @@ $(window).on('scroll', function () {
 
 
 ////////////////////////contact////////////////////////////
-$(function () {
-
-    $("#contactForm").validator().on("submit", function (event) {
-        if (event.isDefaultPrevented()) {
-            // handle the invalid form...
-            formError();
-            submitMSG(false, "Did you fill in the form properly?");
-        } else {
-            // everything looks good!
-            event.preventDefault();
-            submitForm();
-        }
-        })
-        
-});
-
-function submitForm() {
-    // Initiate Variables With Form Content
-    var name = $("#name").val();
-    var email = $("#email").val();
-    var message = $("#message").val();
-
-    $.ajax({
-        type: "POST",
-        url: "contact.php",
-        data: "name=" + name + "&email=" + email + "&message=" + message,
-        success: function (text) {
-            if (text == "success") {
-                formSuccess();
-            } else {
-                formError();
-                submitMSG(false, text);
-            }
-        }
-    });
-}
-
-function formSuccess() {
-    $("#contactForm")[0].reset();
-    submitMSG(true, "Message Submitted!");
-}
-function formError() {
-    $("#contactForm").removeClass().addClass('shake animated').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function () {
-        $(this).removeClass();
-    });
-}
-
-function submitMSG(valid, msg) {
-    var msgClasses;
-    if (valid) {
-        msgClasses = "h3 text-center text-success";
-    } else {
-        msgClasses = "h3 text-center text-danger";
-    }
-    $("#msgSubmit").removeClass().addClass(msgClasses).text(msg);
-};
